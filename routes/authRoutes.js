76fb2b63d7e1232386a4d5ef0e7e3ebeabd30f93
Kevin Christman Lumban Tobing <KEVINTOBINGGAMING@GMAIL.COM>
2025-05-17 22:57:31 +0700

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { isAuthenticated, login, logout, register, resetPassword, sendResetOtp, sendVerifyOtp, verifyEmail } from "../controllers/authController.js";
+import { checkAuth, isAuthenticated, login, logout, register, resetPassword, sendResetOtp, sendVerifyOtp, verifyEmail } from "../controllers/authController.js";
 import userAuth from "../middleware/userAuth.js";
 
 const authRouter = express.Router()
@@ -9,7 +9,7 @@ authRouter.post('/login', login)
 authRouter.post('/logout', logout)
 authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp)
 authRouter.post('/verify-account', userAuth, verifyEmail)
-authRouter.get('/is-auth', userAuth, isAuthenticated)
+authRouter.get('/is-auth', userAuth, isAuthenticated, checkAuth)
 authRouter.post('/send-reset-otp', sendResetOtp)
 authRouter.post('/reset-password', resetPassword)
 
