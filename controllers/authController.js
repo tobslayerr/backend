b5d9afd0856c3625ab5f01f2a9d00b520add27ac
Kevin Christman Lumban Tobing <KEVINTOBINGGAMING@GMAIL.COM>
2025-05-17 23:08:49 +0700

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -31,7 +31,8 @@ export const register = async (req, res)=> {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // Perubahan dari 'strict' ke 'lax'/'none'
-            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 hari dalam milidetik
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            path: '/'
         })
         
         // Sending email
