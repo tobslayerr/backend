f07de08134d158f5f6c78d085b5bb45caa074699
Kevin Christman Lumban Tobing <KEVINTOBINGGAMING@GMAIL.COM>
2025-06-05 02:26:38 +0700

    Update ratingModel.js

--- models/ratingModel.js
+++ models/ratingModel.js
@@ -1,13 +1,12 @@
 import mongoose from 'mongoose';
 
 const ratingSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  event: { type: mongoose.Schema.Types.ObjectId, ref: 'Event'},
-  stars: { type: Number, required: true, min: 1, max: 5 },
-  review: { type: String, default: '' },
-  status: {type: String, enum: ['unrated', 'rated'], 
-    default: 'unrated'
- }, 
-});
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
+  event: { type: mongoose.Schema.Types.ObjectId, ref: "Event", required: true },
+  stars: { type: Number, required: true },
+  review: { type: String },
+}, { timestamps: true });
+
+ratingSchema.index({ user: 1, event: 1 }, { unique: true });
 
 export default mongoose.model("Rating", ratingSchema);
