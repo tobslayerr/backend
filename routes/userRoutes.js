f2d9dcdca2aa90eb23a971ad4dc1be3ed77d305b
unknown <rizkyshakira2017@gmail.com>
2025-06-06 02:22:41 +0700

    menambahkan input map

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import userAuth from '../middleware/userAuth.js';
-import { approveSiCreator, getUserData, rejectSiCreator, requestSiCreator } from '../controllers/userController.js';
+import { approveSiCreator, getUserData, rejectSiCreator, requestSiCreator, getUserTickets } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
@@ -8,5 +8,6 @@ userRouter.get('/data', userAuth, getUserData);
 userRouter.post('/requestsicreator', userAuth, requestSiCreator)
 userRouter.put('/approvesicreator/:userId', approveSiCreator)
 userRouter.put('/rejectsicreator/:userId', rejectSiCreator)
+userRouter.get('/tickets', userAuth, getUserTickets);
 
 export default userRouter;
