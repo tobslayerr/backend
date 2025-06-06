0ad61a590a324fc3c8ec4d14c596870c70c6affa
Iam Ren <liaurentinoneo25@gmail.com>
2025-06-06 15:17:16 +0700

    Update qrRoutes.js

--- routes/qrRoutes.js
+++ routes/qrRoutes.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import { generateTicketQr, verifyQrScan } from '../controllers/qrController.js';
+import {verifyQrScan } from '../controllers/qrController.js';
 import userAuth from '../middleware/userAuth.js';
 
 const qrRouter = express.Router();
 
-qrRouter.get('/generateqr/:ticketId', userAuth, generateTicketQr);
 qrRouter.get('/verifyqr', verifyQrScan);
 
 export default qrRouter;
